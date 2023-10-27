975ae77254d648173ccc963096fd21620f6d0e66
Taehun Kim <mistercoin818@gmail.com>
2023-10-28 01:17:17 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string word;
+int main() {
+	ios::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+    cin >> word;
+    cout << word.length();
+}
