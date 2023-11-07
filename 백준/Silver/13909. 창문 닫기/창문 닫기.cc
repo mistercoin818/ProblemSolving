c6d89b87d4af6fca7185e48a16d720e4ac16e406
Taehun Kim <mistercoin818@gmail.com>
2023-11-07 09:48:37 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int N;
+int main() {
+	ios::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    cout << (int)sqrt(N);
+}
